a173672d8c521649b75800a3233d4dbdbc40c82a
Priyanshi jain <144136140+jainnPriyanshii@users.noreply.github.com>
2024-01-01 21:26:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('fs');
+const replaceThis = "priyanshi"
+const replaceWith = "jain"
+const preview = true
+const folder = __dirname
+try {
+   fs.readdir(folder,"data",(err,data)=>{
+
+    console.log(data);
+    for (let index=0;index<data.length;index++){
+        const item =data[index];
+        let oldFile = path.join(folder+item)
+        let newFile = path.join(folder+item.replaceAll(replaceThis,replaceWith))
+        if(!preview){
+        fs.rename(oldFile, newFile, ()=>{
+            console.log("Rename Success")
+        })
+    }
+    else{
+        if("data/" + item!==newFile)console.log("data/" + item + "will be renamed to" + newFile)
+    }
+
+    }
+
+  })
+  
+} catch (err) {
+  console.error(err);
+}
